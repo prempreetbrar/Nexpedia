a3a7cd375ee552843c89d2cd7b569a696e2a3113
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-04 12:23:30 -0600

    reorganize routes

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/toursRoutes");
+const userRouter = require("./routes/usersRoutes");
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
