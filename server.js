db662dc13f733354aa47507a05b7ac1e3e3d43ff
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-04 20:19:06 -0600

    fixing morgan bug and adding node to package.json

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const dotenv = require("dotenv");
+const ENV = `${__dirname}/config.env`;
+dotenv.config({ path: ENV });
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const ENV = `${__dirname}/config.env`;
-dotenv.config({ path: ENV });
 const PORT = process.env.PORT || 3000;
 const DB = process.env.DATABASE.replace(
   /<PASSWORD>/g,
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB).then((connection) => console.log(connection));
+mongoose
+  .connect(DB)
+  .then((connection) => console.log("Connected to the database!"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
