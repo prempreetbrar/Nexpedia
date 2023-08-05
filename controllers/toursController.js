dac7b2ad045a25a2c40f21173f903e7658a757f1
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-04 20:48:06 -0600

    db connection: getting a single tour

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -17,35 +17,37 @@ exports.createTour = async (request, response) => {
   }
 };
 
-exports.getAllTours = (request, response) => {
-  response.status(200);
-  response.json({
-    status: "success",
-    requestedAt: request.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+exports.getAllTours = async (request, response) => {
+  try {
+    const tours = await Tour.find();
+    response.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    response.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
-exports.getTour = (request, response) => {
-  const { id } = request.params;
-  const tour = tours.find((tour) => tour.id === Number(id));
-
-  if (tour) {
-    response.status(200);
-    response.json({
+exports.getTour = async (request, response) => {
+  try {
+    const tour = await Tour.findById(request.params.id);
+    response.status(200).json({
       status: "success",
       data: {
         tour,
       },
     });
-  } else {
-    response.status(404);
-    response.json({
+  } catch (error) {
+    response.status(404).json({
       status: "fail",
-      message: "Invalid ID",
+      message: error,
     });
   }
 };
