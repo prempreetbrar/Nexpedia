44641e7d52617f67a2fc5a853e297a159456b92f
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-06 00:16:37 -0600

    custom validator to ensure price discount is valid

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -48,7 +49,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price."],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          return value < this.price;
+        },
+        message: "Discount ({VALUE}) must be a NUMBER less than the PRICE.",
+      },
+    },
     summary: {
       type: String,
       required: [true, "A tour must have a description."],
