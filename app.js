f12dc13f37b91d18517454020c5c7dfc2c1b882e
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-06 16:43:07 -0600

    bring back apperror import

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
 const errorController = require("./controllers/errorsController");
 
 const tourRouter = require("./routes/toursRoutes");
 const userRouter = require("./routes/usersRoutes");
-const errorRouter = require("./routes/errorsRoutes");
 
 const app = express();
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
@@ -14,7 +14,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
-app.use("*", (request, response, next) => {
+app.all("*", (request, response, next) => {
   next(new AppError(`Cannot find ${request.originalUrl} on the server!`), 404);
 });
 app.use(errorController);
