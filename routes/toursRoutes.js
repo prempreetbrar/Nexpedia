85deef3ce91f6e9ec3fa32acb439f57a571cbaa9
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-05 22:45:09 -0600

    routes for aggregation testing

--- routes/toursRoutes.js
+++ routes/toursRoutes.js
@@ -8,11 +8,15 @@ const {
   checkID,
   checkBody,
   aliasTopTours,
+  getTourStatistics,
+  getMonthlyPlan,
 } = require("../controllers/toursController");
 
 const router = express.Router();
 router.route("/").get(getAllTours).post(createTour);
 router.route("/top-tours").get(aliasTopTours, getAllTours);
+router.route("/tours-stats").get(getTourStatistics);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
