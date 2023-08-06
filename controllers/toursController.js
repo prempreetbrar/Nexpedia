852603a9182cd60c1e74defbb670958953733468
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-05 19:14:37 -0600

    refactoring API Features

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.createTour = async (request, response) => {
   try {
@@ -27,50 +28,17 @@ exports.aliasTopTours = (request, response, next) => {
 
 exports.getAllTours = async (request, response) => {
   try {
-    // special
-    const excludedQuery = { ...request.query };
-    const excludedParams = ["page", "sort", "limit", "fields"];
-    excludedParams.forEach((param) => delete excludedQuery[param]);
-
-    // advanced filtering
-    let filteredQuery = JSON.stringify(excludedQuery);
-    filteredQuery = filteredQuery.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-    filteredQuery = Tour.find(JSON.parse(filteredQuery));
-
-    // sorting
-    const sortString = request.query.sort
-      ? request.query.sort.replaceAll(",", " ")
-      : "-createdAt _id";
-    const fAndSQuery = filteredQuery.sort(sortString);
-
-    // projection
-    const projectionString = request.query.fields
-      ? request.query.fields.replaceAll(",", " ")
-      : "-__v";
-    const fAndSAndPQuery = fAndSQuery.select(projectionString);
-
-    // pagination
-    const pageNumber = Number(request.query.page) || 1;
-    const resultsPerPage = Number(request.query.limit) || 100;
-    const numPages = Math.ceil(
-      (await fAndSAndPQuery.countDocuments()) / resultsPerPage
-    );
-    if (pageNumber > numPages) throw new Error("This page does not exist.");
-
-    const paginatedQuery = fAndSAndPQuery
-      .skip((pageNumber - 1) * resultsPerPage) // skip the results on all previous pages
-      .limit(resultsPerPage); // show the limit on the current page
-
-    // execute the query
-    const tours = await Tour.find(paginatedQuery);
+    const cleanedTours = new APIFeatures(request.query, Tour.find())
+      .filter()
+      .sort()
+      .project()
+      .paginate();
+    const tours = await cleanedTours.dbQuery;
 
     response.status(200).json({
       status: "success",
-      page: pageNumber,
-      results: tours.length,
+      page: cleanedTours.pageNumber,
+      result: tours.length,
       data: {
         tours,
       },
