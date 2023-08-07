0f07116e09abf6fc4ec90a485b3a923bb271dd2e
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-07 10:56:15 -0600

    fix to prevent user from giving themselves a role

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,13 @@ const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.signUpUser = catchAsync(async (request, response) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm,
+  });
+
   response.status(201).json({
     status: "success",
     data: {
