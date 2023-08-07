09f4cd17b405dd808b0cf944fc27e291d58a7aae
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-06 19:50:10 -0600

    handling mongooses duplicate error

--- controllers/errorsController.js
+++ controllers/errorsController.js
@@ -1,4 +1,5 @@
 const AppError = require("../utils/appError");
+const DUPLICATE = 11000;
 
 module.exports = (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
@@ -12,22 +13,25 @@ module.exports = (error, request, response, next) => {
         detailedError = handleDBCastError(detailedError);
         break;
     }
+    switch (error.code) {
+      case DUPLICATE:
+        detailedError = handleDBDuplicateError(detailedError);
+    }
 
     sendErrorProd(detailedError, response);
   }
-
-  // if (!error.isOperational) sendErrorNonOperational(error, response);
-  // switch (process.env.NODE_ENV) {
-  //   case "development":
-  //     sendErrorDev(error, response);
-  //   default:
-  //     sendErrorProd(error, response);
-  // }
 };
 
 function handleDBCastError(error) {
   const message = `Invalid ${error.path}: ${error.value}`;
   return new AppError(message, 400);
+}
+
+function handleDBDuplicateError(error) {
+  const message = `DUPLICATE FIELD. ${Object.keys(error.keyValue)[0]}: ${
+    Object.values(error.keyValue)[0]
+  }. Please use another ${Object.keys(error.keyValue)[0]}.`;
+  return new AppError(message, 404);
 }
 
 function sendErrorDev(error, response) {
