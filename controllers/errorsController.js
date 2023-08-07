7e7f99b09acb64618c17988ec3cd341f42b865e5
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-06 19:13:29 -0600

    Handling Invalid Tour ID

--- controllers/errorsController.js
+++ controllers/errorsController.js
@@ -1,15 +1,34 @@
+const AppError = require("../utils/appError");
+
 module.exports = (error, request, response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || "error";
 
-  if (!error.isOperational) sendErrorNonOperational(error, response);
-  switch (process.env.NODE_ENV) {
-    case "development":
-      sendErrorDev(error, response);
-    default:
-      sendErrorProd(error, response);
+  if (process.env.NODE_ENV === "production") {
+    let detailedError = { ...error };
+
+    switch (error.name) {
+      case "CastError":
+        detailedError = handleDBCastError(detailedError);
+        break;
+    }
+
+    sendErrorProd(detailedError, response);
   }
+
+  // if (!error.isOperational) sendErrorNonOperational(error, response);
+  // switch (process.env.NODE_ENV) {
+  //   case "development":
+  //     sendErrorDev(error, response);
+  //   default:
+  //     sendErrorProd(error, response);
+  // }
 };
+
+function handleDBCastError(error) {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  return new AppError(message, 400);
+}
 
 function sendErrorDev(error, response) {
   response.status(error.statusCode).json({
