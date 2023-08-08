d99ed193b8830af6a016987c8fd8eabb06feb29a
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-07 22:29:27 -0600

    protecting against http parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const errorController = require("./controllers/errorsController");
@@ -28,9 +29,21 @@ app.use(helmet()); // http response header protection
 app.use("/api", limiter);
 app.use(express.json({ limit: "50kb" }));
 
-// data sanitization against NoSQL query injection
+// data sanitization against NoSQL query injection and XSS
 app.use(mongoSanitize());
 app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
