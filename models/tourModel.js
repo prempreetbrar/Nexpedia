9b994bb69280d50de74ffaf9a72b44dbc87053e6
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-11 16:05:27 -0600

    populating Users on Tour find

--- models/tourModel.js
+++ models/tourModel.js
@@ -127,6 +127,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
+  next();
+});
+
 tourSchema.pre("aggregate", function (next) {
   this._pipeline.unshift({
     $match: {
