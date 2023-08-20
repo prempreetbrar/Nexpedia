e2e4fd8447c5df570ecbc866c17831ccd876db5c
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-20 00:32:04 -0600

    add tour page (without map)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,14 @@ exports.getOverview = catchAsync(async (request, response) => {
   });
 });
 
-exports.getTour = (request, response) => {
-  response.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
+exports.getTour = catchAsync(async (request, response) => {
+  const tour = await Tour.findOne({ slug: request.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user",
   });
-};
+
+  response.status(200).render("tour", {
+    title: tour.name,
+    tour,
+  });
+});
