8128528ebe91c75740936ccb0c8734a519504fa8
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-19 22:11:48 -0600

    get all tours on overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (request, response) => {
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (request, response) => {
+  const tours = await Tour.find();
+
   response.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
 exports.getTour = (request, response) => {
   response.status(200).render("tour", {
