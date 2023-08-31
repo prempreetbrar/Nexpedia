bdec11dec44164c08cfa3a622fd1d491ea86b12c
Prempreet Brar <89614923+prempreetbrar@users.noreply.github.com>
2023-08-30 22:22:02 -0600

    clean up server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// we need this to be at the top so we can deal with EVERY unhandledException
 const handleUncaught = require("./utils/handleUncaught");
 handleUncaught("unhandledException");
 
@@ -5,8 +6,9 @@ const dotenv = require("dotenv");
 const ENV = `${__dirname}/config.env`;
 dotenv.config({ path: ENV });
 const mongoose = require("mongoose");
-const app = require("./app");
 
+// app comes after dotenv config so that it has access to .env variables
+const app = require("./app");
 const PORT = process.env.PORT || 3000;
 const DB = process.env.DATABASE.replace(
   /<PASSWORD>/g,
@@ -22,6 +24,7 @@ const server = app.listen(PORT, () => {
 });
 
 handleUncaught("unhandledRejection", server);
+
 process.on("SIGTERM", () => {
   console.log("SIGTERM RECEIVED; shutting down gracefully.");
   server.close(() => console.log("Server shut down complete."));
